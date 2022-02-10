cf78c34374ff5a84dc43718e1ddef8680f7eafbf
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-02-09 23:35:11 -0500

    create notes component, simplify code using ES6 features

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "./notes";
 
 function App() {
     return <div>
                 <Header />
-                <Note />
+                {notes.map(noteItem =>  (
+                        <Note 
+                            key={noteItem.id} 
+                            title={noteItem.title} 
+                            content={noteItem.content} 
+                        />
+                    )
+                )}
                 <Footer />
             </div>
 }
