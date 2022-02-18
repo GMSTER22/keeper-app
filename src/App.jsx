82ddbcfd87229c3e4f88d3291e0e5287c53f44bf
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-02-17 19:08:01 -0500

    create Input component, manage state with useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import Input from "./Input"
 import notes from "./notes";
 
 function App() {
+    function addNote(note) {
+        console.log(notes, "first")
+        notes.push(note);
+        console.log(notes, "second")
+    }
+
+
     return <div>
                 <Header />
+                <Input handleNote={addNote} />
                 {notes.map(noteItem =>  (
                         <Note 
                             key={noteItem.id} 
---
+++ src/Input.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+
+function Input(props) {
+
+    const [ note, setNote ] = useState( { 
+        title: "",
+        content: ""
+    });
+    
+    
+    
+    function handleChange(event) {
+        const { name, value } = event.target;
+        console.log(name, value);
+        setNote(prevNote => {
+            return { 
+                ...prevNote,
+                [name]: value
+            }
+        })
+    }
+
+    return (
+        <form className="input">
+            <input onChange={handleChange} className="input-title" type="text" name="title" placeholder="title" value={note.title} />
+            <input onChange={handleChange} className="input-content" name="content" type="text" placeholder="Take note..." value={note.content} />
+            <button onClick={(e)=> {
+                e.preventDefault();
+
+                props.handleNote(note);
+                setNote({
+                    title: "",
+                    content: ""
+                });
+            }} className="btn btn-add" type="submit">Add</button>            
+        </form>
+    )
+}
+
+export default Input;
