42c22c8811e01143de6f9a566c03ae450b0d8aa4
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-03-31 13:33:46 -0400

    Updates

---
+++ precache-manifest.82b3653e8c85034a0eb237d9a372a623.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "03f804ada1cdbcad1db4",
+    "url": "/keeper-app/static/js/main.d472f53c.chunk.js"
+  },
+  {
+    "revision": "7491953a57bd06d42130",
+    "url": "/keeper-app/static/js/runtime~main.afea614e.js"
+  },
+  {
+    "revision": "bd5752ade229407adea2",
+    "url": "/keeper-app/static/js/2.b2bd2905.chunk.js"
+  },
+  {
+    "revision": "9762cb867d0a057c556b67f8600183dc",
+    "url": "/keeper-app/index.html"
+  }
+];
